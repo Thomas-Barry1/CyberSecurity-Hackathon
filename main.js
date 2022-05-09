ad48417b7a738c99aa4b4a137b542cd8bd972327
Jainam213 <102665090+Jainam213@users.noreply.github.com>
2022-05-10 01:05:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function submit() {
   };
  fetch("https://social-scanner.p.rapidapi.com/social-scan/", options)
     .then((response) => response.json()) 
-  var jsonArray = [{response.json()}]
+  var jsonArray = [response.json()]
   let name = jsonArray[0].valueInput;
 https = JSON.parse(https);
 https.data.forEach(value => {
