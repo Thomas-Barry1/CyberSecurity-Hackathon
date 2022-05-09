cd5f904a1f8dd07c9a65fb49158512c9dc4afe32
Thomas Barry <54588947+Thomas-Barry1@users.noreply.github.com>
2022-05-09 10:46:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-document.getElementById("addBtn").addEventListener("click", addFields);
 var elements = document.getElementsByClassName("drop-js");
+
 for (var index = 0; index < elements.length; index++) {
   elements[index].addEventListener("click", changeDrop);
 }
+document.getElementById("addBtn").addEventListener("click", addFields);
 
 function changeDrop() {
   document.getElementById("dropdownMenuButton1").innerText = this.innerText;
@@ -10,11 +11,35 @@ function changeDrop() {
 }
 
 function addFields() {
-  const div = document.createElement("div");
-  var tag = document.createElement("p");
-  var text = document.createTextNode("Tutorix is the best e-learning platform");
-  tag.appendChild(text);
-  var element = document.getElementById("new");
-  element.appendChild(tag);
+  var elem = document.querySelector("#emailAndBtn");
+  var elemClone = elem.cloneNode(true);
+  elemClone.id = "#emailAndBtn2";
+  elem.after(elemClone);
+  return;
+}
+
+document.querySelector("#formSubmit").addEventListener("click", submit);
+
+function submit() {
+  const encodedParams = new URLSearchParams();
+  const user = document.getElementById("staticEmail2").value;
+  encodedParams.append("username", String(user));
+  encodedParams.append("target_count", "10");
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Host": "social-scanner.p.rapidapi.com",
+      "X-RapidAPI-Key": "f12d0742e8msh3bc11e1c22b35fbp174ae3jsn50469845b8c2",
+    },
+    body: encodedParams,
+  };
+
+  fetch("https://social-scanner.p.rapidapi.com/social-scan/", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
   return;
 }
