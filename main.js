5ac3b37171af66760337677064f41b7a1eaae98d
Jainam213 <102665090+Jainam213@users.noreply.github.com>
2022-05-10 00:39:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
 var elements = document.getElementsByClassName("drop-js");
 
 for (var index = 0; index < elements.length; index++) {
@@ -39,15 +39,7 @@ function submit() {
   };
 char linkArray[1000];
   fetch("https://social-scanner.p.rapidapi.com/social-scan/", options)
-    .then((response) => response.json())
-     .then((response) => String jsonString)
-      ObjectMapper mapper = new ObjectMapper();
-      ObjectNode node = mapper.readValue(jsonString, ObjectNode.class);
-      if(node.has("https")) {
-         System.out.println("link" + node.get("https"));
-        linkArray[1] = node.get("https");
-      }
-   
+    .then((response) => response.json())                                 
     .then((response) => console.log(response)) 
     .catch((err) => console.error(err));
 
